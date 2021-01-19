9d31d18500c96c42086d8c3b392f70c8c6d327fc
MMD <sirmmds@gmail.com>
2021-01-19 10:19:27 +0330

    import get_coinbase_trades in get_trades

--- trades/management/commands/update_trades.py
+++ trades/management/commands/update_trades.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand
+
+from trades.fetchTrades.coinbase.get_cointbase_trades import get_coinbase_trades
 from trades.fetchTrades.deus.get_deus_trades import get_deus_trades
 from trades.models import Currency
 from trades.update_trades import UpdateTrades
