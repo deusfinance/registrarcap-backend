5139763a6c5d84defe7a8a2713a6db3fa3366517
MMD <sirmmds@gmail.com>
2021-01-20 18:47:33 +0330

    add search to admin for trade

--- trades/admin.py
+++ trades/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from trades.models import Candlestick, Trade, Currency
 
+
+class TradeAdmin(admin.ModelAdmin):
+    search_fields = ('id', 'timestamp', 'amount', 'hash')
+
+
 admin.site.register(Currency)
 admin.site.register(Candlestick)
-admin.site.register(Trade)
+admin.site.register(Trade, TradeAdmin)
