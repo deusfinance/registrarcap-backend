563d5c0fb971bab8a45c4d13ba4b8411e1e6955c
MMD <sirmmds@gmail.com>
2021-02-02 17:43:00 +0330

    update dr collector api

--- trades/management/commands/update_dea_price.py
+++ trades/management/commands/update_dea_price.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
         url = "https://dr-collector-api.herokuapp.com/v1/transactions?poolContract=0x92adab6d8dc13dbd9052b291cfc1d07888299d65&from={}&to={}"
         from_timestamp = 0
         to_timestamp = 99999999999999
-        dea_deus_swaps = requests.get(url.format(from_timestamp, to_timestamp)).json()
+        dea_deus_swaps = requests.get(url.format(from_timestamp, to_timestamp)).json()['results']
         deus_to_dea = [(s['timestamp'], 1 / s['price']) for s in dea_deus_swaps]
 
         for trade in Trade.objects.filter(timestamp__gte=deus_to_dea[0][0]).all():
