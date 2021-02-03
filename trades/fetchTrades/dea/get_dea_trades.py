d58481c7a74fb212cdf98c249eff61c211870928
MMD <sirmmds@gmail.com>
2021-02-03 10:04:23 +0330

    update get_dea_trades

--- trades/fetchTrades/dea/get_dea_trades.py
+++ trades/fetchTrades/dea/get_dea_trades.py
@@ -5,7 +5,7 @@ from trades.models import Trade
 def get_dea_trades(from_block, limit):
     last_trade = Trade.objects.filter(currency__symbol='dea').order_by('-timestamp').first()
     if last_trade:
-        from_timestamp = last_trade.timestamp - 1
+        from_timestamp = last_trade.timestamp + 1
     else:
         from_timestamp = 0
     url = "https://dr-collector-api.herokuapp.com/v1/transactions?poolContract=0x92adab6d8dc13dbd9052b291cfc1d07888299d65&from={}&to={}"
