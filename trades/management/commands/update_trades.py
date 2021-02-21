4a121f21a28b58ed564e2fc93e8ef6994dbd8f28
MMD <sirmmds@gmail.com>
2021-02-21 08:54:02 +0330

    add telegram alert for script debugging

--- trades/management/commands/update_trades.py
+++ trades/management/commands/update_trades.py
@@ -1,10 +1,14 @@
-from django.core.management.base import BaseCommand
+import datetime
 
+from django.core.management.base import BaseCommand
+from pyrogram import Client
+
+from backend.local_settings import TELEGRAM_APP_ID, TELEGRAM_API_HASH, TELEGRAM_PRICE_BOT_TOKEN
 from trades.fetchTrades.bakkt.get_bakkt_trades import get_bakkt_trades
 from trades.fetchTrades.coinbase.get_cointbase_trades import get_coinbase_trades
 from trades.fetchTrades.dea.get_dea_trades import get_dea_trades
 from trades.fetchTrades.deus.get_deus_trades import get_deus_trades
-from trades.models import Currency
+from trades.models import Currency, Trade
 from trades.update_trades import UpdateTrades
 
 
@@ -12,6 +16,21 @@ class Command(BaseCommand):
     help = 'update trades'
 
     def handle(self, *args, **options):
+        last_trade = Trade.objects.first()
+        if last_trade.timestamp + 12 * 60 * 60 > datetime.datetime.now().timestamp():
+            app = Client(
+                "bot",
+                api_id=TELEGRAM_APP_ID,
+                api_hash=TELEGRAM_API_HASH,
+                bot_token=TELEGRAM_PRICE_BOT_TOKEN
+            )
+            app.start()
+            app.send_message(
+                'mmdmst',
+                "There is a problem with chart scripts, last trade id: {}".format(last_trade.id)
+            )
+            app.stop()
+
         currency = Currency.objects.get(symbol='deus')
         update_trades = UpdateTrades(currency, get_deus_trades)
         update_trades.update()
